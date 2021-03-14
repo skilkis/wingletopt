dd3da6d54d84b9d069f7998e89d2aea6cd12dd8b
skilkis <sankilkis@msn.com>
2021-03-14 22:17:56 +0100

    Improve avlwrapper test using Kinga Budziak, 2015

--- tests/test_wingletopt/test_avl/test_avl.py
+++ tests/test_wingletopt/test_avl/test_avl.py
@@ -1,44 +1,55 @@
-import math
 from typing import List
 
 import pytest
 
 from wingletopt import avl
 
+CHORD = 1.8
 
-@pytest.fixture()
-def surface() -> avl.Surface:
-    """Quasi-infinite wing surface with chord = 1 m, span = 2000 m."""
-    root_section = avl.Section(
-        leading_edge_point=avl.Point(0, 0, 0),
-        chord=1,
-        airfoil=avl.NacaAirfoil("0012"),
-    )
-    tip_section = avl.Section(
-        leading_edge_point=avl.Point(0, 1e3, 0),
-        chord=1,
-        airfoil=avl.NacaAirfoil("0012"),
+
+@pytest.fixture
+def span(aspect: float) -> float:
+    """Calculates the span required to satisfy ``aspect``."""
+    return CHORD * aspect
+
+
+@pytest.fixture
+def area(span: float, aspect: float) -> float:
+    """Calculates the surface area using ``span`` and ``aspect``."""
+    return span ** 2 / aspect
+
+
+@pytest.fixture
+def surface(aspect: float) -> avl.Surface:
+    """Defines the rectangular wing used by (Budziak, 2015).
+
+    pg. 37-41 of Kinga Budziak, 2015:
+    https://www.fzt.haw-hamburg.de/pers/Scholz/arbeiten/TextBudziak.pdf
+    """
+    root = avl.Section(leading_edge_point=avl.Point(0, 0, 0), chord=CHORD)
+    tip = avl.Section(
+        leading_edge_point=avl.Point(0, aspect * CHORD / 2, 0), chord=CHORD
     )
     return avl.Surface(
         name="wing",
-        n_chordwise=20,
+        n_chordwise=8,
         chord_spacing=avl.Spacing.cosine,
-        n_spanwise=20,
-        span_spacing=avl.Spacing.cosine,
+        n_spanwise=12,
+        span_spacing=avl.Spacing.neg_sine,
         y_duplicate=0.0,
-        sections=[root_section, tip_section],
+        sections=[root, tip],
     )
 
 
 @pytest.fixture
-def geometry(surface: avl.Surface) -> avl.Geometry:
+def aircraft(surface: avl.Surface, area: float, span: float) -> avl.Aircraft:
     """Sample test geometry for AVL."""
-    return avl.Geometry(
-        name="test_wing",
-        reference_area=2e3,
-        reference_chord=1,
-        reference_span=2e3,
-        reference_point=avl.Point(0, 0, 0),
+    return avl.Aircraft(
+        name="test_aircraft",
+        reference_area=area,
+        reference_chord=CHORD,
+        reference_span=span,
+        reference_point=avl.Point(0.54, 0, 0),
         surfaces=[surface],
     )
 
@@ -49,28 +60,35 @@ def cases(alpha) -> List[avl.Case]:
     return [avl.Case(name=f"alpha={alpha}", alpha=alpha)]
 
 
-def test_avl_monkeypatch(geometry: avl.Geometry) -> None:
+@pytest.mark.parametrize("aspect", [10])
+def test_avl_monkeypatch(aircraft: avl.Aircraft, tmpdir) -> None:
     """Tests if a :py:class:`avl.Session` can be run without config."""
-    avl_session = avl.Session(geometry, cases=[avl.Case("test_case", alpha=0)])
-    avl_session.run_all_cases()  # This should succeed
+    avl_session = avl.Session(aircraft, cases=[avl.Case("test_case", alpha=0)])
+    avl_session.run_all_cases(directory=tmpdir)  # This should succeed
 
 
-@pytest.fixture
-def expected_lift(alpha: float) -> float:
-    """Calculates the expected infinite span wing lift coefficient."""
-    return 2 * math.pi * math.radians(alpha)
-
-
-@pytest.mark.parametrize("alpha", [0, 5, 10])
-def test_avl_lift(geometry, cases, expected_lift):
+# Testing using values from pg. 41 of Kinga Budziak, 2015
+@pytest.mark.parametrize(
+    "aspect, alpha, oswald, expected_drag, expected_lift",
+    (
+        (5, 6.91, 0.9892, 0.01456, 0.47565),
+        (7, 6.17, 0.9779, 0.01049, 0.47503),
+        (9, 5.77, 0.9656, 0.00825, 0.47473),
+        (10, 5.63, 0.9594, 0.00747, 0.47463),
+        (11, 5.52, 0.9531, 0.00684, 0.47456),
+        (13, 5.34, 0.9413, 0.00586, 0.47445),
+        (15, 5.21, 0.9299, 0.00514, 0.47437),
+    ),
+)
+def test_avl_lift(
+    aircraft: avl.Aircraft, cases, oswald, expected_drag, expected_lift
+):
     """Tests AVL output for correct lift coefficient and gradient."""
-    avl_session = avl.Session(geometry, cases=cases)
+    avl_session = avl.Session(aircraft, cases=cases)
     result = avl_session.run_all_cases()[cases[0].name]
 
     # Checking if CL value is as expected
-    assert result["Totals"]["CLtot"] == pytest.approx(expected_lift, rel=0.1)
-
-    # Checking if CLa is close to 2π
-    assert result["StabilityDerivatives"]["CLa"] == pytest.approx(
-        2 * math.pi, rel=0.1
-    )
+    # assert result["Totals"][] ==
+    assert result["Totals"]["e"] == pytest.approx(oswald, rel=1e-3)
+    assert result["Totals"]["CDff"] == pytest.approx(expected_drag, rel=1e-2)
+    assert result["Totals"]["CLff"] == pytest.approx(expected_lift, rel=1e-2)
